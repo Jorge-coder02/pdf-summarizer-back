12251f48bf456d24c81e977b305db20153ed2fda
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-07 15:48:16 +0200

    fix: package json ruta index.js mal

--- server-test.js
+++ server-test.js
@@ -7,6 +7,6 @@ app.get("/", (_, res) => {
   res.json({ msg: "Servidor test OK" });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Servidor test corriendo en puerto ${PORT}`);
 });
