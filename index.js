293f71723f0bf60909f8851b786744f86b5b22b7
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-07 14:47:48 +0200

    feat: nuevo test ruta get

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors()); // Sin opciones, permite todo y agrega el header
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
