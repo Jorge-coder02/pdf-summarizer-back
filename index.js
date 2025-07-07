3877d39de69ababa74138d52a43ac2cd7d7f016c
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-07 15:00:37 +0200

    feat: nuevo test ruta get

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: "*", // Permite todos los orígenes (solo para desarrollo)
     methods: ["GET", "POST"],
     credentials: true,
   })
