4a4398867e1ba36e495bcfe2f7dcc36e3e086381
Jorge-coder02 <jorgitenis@hotmail.es>
2025-07-07 14:36:42 +0200

    feat: nuevo test ruta get

--- index.js
+++ index.js
@@ -12,7 +12,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    credentials: true, // si usas cookies o auth con credenciales
+  })
+);
 app.use(express.json());
 
 // Multer config para guardar en 'uploads/'
